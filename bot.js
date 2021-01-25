a99b9b17228df3bc9ae2896f59cb83cae0012041
coleW <cole.woodyard@gmail.com>
2021-01-24 23:13:40 -0500

    Fixed a memory leak and added player listing

--- bot.js
+++ bot.js
@@ -1,30 +1,18 @@
 /* Author: v1p3r_hax
 *  Description: A pourpose built discord bot to get the server status of the SMP server used in the streams
-*  Version: 1.0.1
+*  Version: 1.0.2
 */
 const discord = require('discord.js')
 const util = require('minecraft-server-util');
 const config = require('./config.json');
 const statBot = new discord.Client();
 
-const REFRESH_INTERVAL = 0.5 * 60 * 1000;
-
 statBot.on('ready', async() => {
     setDefaultStatus();
     console.log("Standing ready sire!");
-    serverPingStat();
-});
-
-function setDefaultStatus(){
-    statBot.on('presenceUpdate', async() => {
-        statBot.user.setStatus("online");
-    });
-};
-
-async function serverPingStat(){
-    while(config.stopper == false){
-        setDefaultStatus();
-        await util.status(config.ip, {port: config.port})
+    //setting up a reoccuring check of the server to change the bot status
+    setInterval(function (){
+        util.status(config.ip, {port: config.port})
         .then((result) => {
             statBot.user.setStatus("online");
             console.log("yea, we up!");
@@ -33,30 +21,41 @@ async function serverPingStat(){
             statBot.user.setStatus("dnd");
             console.log("WE GOING DOWN!");
             throw error;
-        });
-        await new Promise(done => setTimeout(done, REFRESH_INTERVAL));
-    };
+        })
+    }, 3000);
+});
+
+function setDefaultStatus(){
+        statBot.user.setStatus("online");
 };
 
 statBot.on('message', async message => {
     switch(message.content){
         case 'v-status':
-            util.status(config.ip,{ port: config.port })
+            util.queryFull(config.ip,{ port: config.port })
             .then((response) => {
+                currPlayers = ' ';
+                onlinePlaying = response.onlinePlayers;
+                if(!onlinePlaying == 0){
+                    currPlayers = response.players;
+                } else {
+                    currPlayers = "Empty";
+                };
                 const Embed = new discord.MessageEmbed()
                     .setTitle('Server Status')
                     .addField('Server IP', response.host)
                     .addField('Server Version', response.version)
                     .addField('Online Players', response.onlinePlayers)
                     .addField('Max Players', response.maxPlayers)
+                    .addField('Players', currPlayers)
                     message.channel.send(Embed)
                     console.log(response);
             })
             
-            if(error){
+            .catch((error) => {
                 message.channel.send("Server Denied the Request!");
                 throw error;
-            } 
+            }) 
             break;
             
         case 'v-modpack':
