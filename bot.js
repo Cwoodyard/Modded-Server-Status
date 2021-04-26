94e19809727b894ce2675dbe04a98ce5e07b71a9
coleW <cole.woodyard@gmail.com>
2021-04-26 17:24:43 -0400

    Fixed the repeating bs it was doing. Added a debug too.

--- bot.js
+++ bot.js
@@ -30,70 +30,69 @@ function setDefaultStatus() {
 };
 
 statBot.on('message', async message => {
-    switch (message.content) {
-        case 'v-status':
-            util.queryFull(config.ip, { port: config.port })
-                .then((response) => {
-                    currPlayers = ' ';
-                    onlinePlaying = response.onlinePlayers;
-                    if (!onlinePlaying == 0) {
-                        currPlayers = response.players;
-                    } else {
-                        currPlayers = "Empty";
-                    };
-                    const Embed = new discord.MessageEmbed()
-                        .setTitle('Server Status')
-                        .addField('Server IP', response.host)
-                        .addField('Server Version', response.version)
-                        .addField('Online Players', response.onlinePlayers)
-                        .addField('Max Players', response.maxPlayers)
-                        .addField('Players', currPlayers)
-                    message.channel.send(Embed)
-                    console.log(response);
+    // message.channel.send(message.content.slice(0, 2));
+
+    if (message.content.startsWith('v-')) {
+        switch (message.content.slice(2, message.content.length)) {
+            case 'status':
+                util.queryFull(config.ip, { port: config.port })
+                    .then((response) => {
+                        currPlayers = ' ';
+                        onlinePlaying = response.onlinePlayers;
+                        if (!onlinePlaying == 0) {
+                            currPlayers = response.players;
+                        } else {
+                            currPlayers = "Empty";
+                        };
+                        const Embed = new discord.MessageEmbed()
+                            .setTitle('Server Status')
+                            .addField('Server IP', response.host)
+                            .addField('Server Version', response.version)
+                            .addField('Online Players', response.onlinePlayers)
+                            .addField('Max Players', response.maxPlayers)
+                            .addField('Players', currPlayers)
+                        message.channel.send(Embed)
+                        console.log(response);
+                    })
+
+                .catch((error) => {
+                    message.channel.send("Server Denied the Request!");
+                    throw error;
                 })
+                break;
 
-            .catch((error) => {
-                message.channel.send("Server Denied the Request!");
-                throw error;
-            })
-            break;
+            case 'MVSMPmodpack':
+                message.channel.send('Potion Shop Madness:\thttps://www.curseforge.com/minecraft/modpacks/potion-shop-madness');
+                break;
 
-        case 'v-MVSMPmodpack':
-            message.channel.send('Potion Shop Madness:\thttps://www.curseforge.com/minecraft/modpacks/potion-shop-madness');
-            break;
+            case 'FAmodpack':
+                message.channel.send("Mana's Fabric Adventures:\thttps://www.curseforge.com/minecraft/modpacks/manas-fabric-adventures");
+                break;
 
-        case 'v-FAmodpack':
-            message.channel.send("Mana's Fabric Adventures:\thttps://www.curseforge.com/minecraft/modpacks/manas-fabric-adventures");
-            break;
+            case 'check':
+                serverPingStat();
+                break;
 
-        case 'v-check':
-            serverPingStat();
-            break;
+            case 'help':
+                message.channel.send('Complain To @ManaHakume#8416 and use https://www.google.com/');
+                break;
 
-        case 'v-help':
-            message.channel.send('Complain To @ManaHakume#8416 and use https://www.google.com/');
-            break;
+            case 'commands':
 
-        case 'v-commands':
-
-            const Embed2 = new discord.MessageEmbed()
-                .setTitle('Commands')
-                .addField('v-status', "Command to get the Fabric minecraft server status.")
-                .addField('v-check', "Similar to v-status where it check to see of server is online.")
-                .addField('v-MVSMPmodpack', "Where to download the Forge modpack.")
-                .addField('v-FAmodpack', "Where to download the Fabric Modpack.")
-                .addField('v-help', "What to do if you need help.")
-                .addField('v-commands', "This prompt.")
-            message.channel.send(Embed2)
-            break;
-        default:
-            message.channel.send('invalid Command. Please type v-commands for the list of commands.')
-            break;
-
-
-
-
-
+                const Embed2 = new discord.MessageEmbed()
+                    .setTitle('Commands')
+                    .addField('v-status', "Command to get the Fabric minecraft server status.")
+                    .addField('v-check', "Similar to v-status where it check to see of server is online.")
+                    .addField('v-MVSMPmodpack', "Where to download the Forge modpack.")
+                    .addField('v-FAmodpack', "Where to download the Fabric Modpack.")
+                    .addField('v-help', "What to do if you need help.")
+                    .addField('v-commands', "This prompt.")
+                message.channel.send(Embed2)
+                break;
+            default:
+                message.channel.send('invalid Command. Please type v-commands for the list of commands.')
+                break;
+        }
     }
 });
 
